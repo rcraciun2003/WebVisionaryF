dc7b359810c26163f560af4bb5342b1617c1612b
Aleksandruu <florin.mischie@student.upt.ro>
2022-07-26 16:53:29 +0300

    nav done but mobile

--- src/sections/Nav.js
+++ src/sections/Nav.js
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo.png";
 import { Button } from "./components/Button";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { FaBars } from "react-icons/fa";
 export const Nav = () => {
+  const [dropdown, setDropdown] = useState(false);
   return (
-    <div className="">
-      <div>
-        <img src={logo}></img>
-        <a>Acasa</a>
-        <a>Despre Noi</a>
-        <button> Servicii</button>
-        <a>Portofoliu</a>
-        <a>Contact</a>
+    <div className="flex w-screen justify-between font-medium fixed bg-white">
+      <img src={logo} className="h-16 ml-6 pr-14"></img>
+
+      <div className="flex justify-center align-center hidden lg:flex">
+        <a className="h-16 ml-6 pt-6">Acasa</a>
+        <a className="h-16 ml-6 pt-6">Despre Noi</a>
+        <div className="h-16 ml-4 pt-6">
+          <button
+            onClick={() => {
+              setDropdown(!dropdown);
+            }}
+            className="flex"
+          >
+            Servicii
+            <RiArrowDropDownLine className="pt-1" />
+          </button>
+          {dropdown && (
+            <div className="absolute rounded-b-lg flex flex-col">
+              <a className="pt-2">Creeare WebSite</a>
+              <a className="pt-2">Creeare Magazin Online</a>
+              <a className="pt-2">Creeare Aplicatie Web</a>
+              <a className="pt-2">Opttimizare SEO</a>
+            </div>
+          )}
+        </div>
+        <a className="h-16 ml-6 pt-6">Portofoliu</a>
+        <a className="h-16 ml-6 pt-6">Contact</a>
       </div>
-      <div>
-        <p>+40720404104</p>
-        <Button>Hai Sa Vorbim</Button>
+      <div className="flex justify-center align-center pt-6 mr-10  hidden lg:flex">
+        <p className="mr-6">+40720404104</p>
+        <Button text="Hai Sa Vorbim"></Button>
       </div>
+      <button>
+        <FaBars className="h-14 mr-6" />
+      </button>
     </div>
   );
 };
--- src/sections/components/Button.js
+++ src/sections/components/Button.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export const Button = () => {
-  return <div>Button</div>;
+export const Button = ({ text }) => {
+  return (
+    <div
+      style={{
+        background:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,105,1) 0%, rgba(162,0,255,1) 100%)",
+        borderRadius: "15px",
+        height: "30px",
+        padding: "2px 20px",
+        color: "white",
+      }}
+    >
+      <p>{text}</p>
+    </div>
+  );
 };
