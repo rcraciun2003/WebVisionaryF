b990a135e7951e9f77334b97cb15c63584fbf1c4
rcraciun2003 <craciun.razvan2003@yahoo.com>
2022-07-26 14:14:31 +0300

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+
+    import React from "react";
+    import logo from '../assets/images/logo.png';
+export const Nav = () => {
+  return (
+    <div className="">
+      <div>
+        <img src={logo}></img>
+        <a>Acasa</a>
+        <a>Despre Noi</a>
+        <button> Servicii</button>
+        <a>Portofoliu</a>
+        <a>Contact</a>
+      </div>
+      <div>
+        <p>+40720404104</p>
+        {/* <Button>Hai Sa Vorbim</Button> */}
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Nav } from "../components/Nav";
+import { HeaderHome } from "../components/HeaderHome";
+export const Homepage = () => {
+  return (
+    <>
+      <Nav></Nav>
+      <HeaderHome></HeaderHome>
+      {/* <WhyPickUs></WhyPickUs>
+      <European></European>
+      <Services></Services>
+      <Portofolio></Portofolio>
+      <Testiomonials></Testiomonials>
+      <Footer></Footer> */}
+    </>
+  );
+};
