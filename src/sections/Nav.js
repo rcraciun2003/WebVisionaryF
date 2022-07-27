51baca24af97fc3f7a11515352b06f70505e4c70
Aleksandruu <florin.mischie@student.upt.ro>
2022-07-27 14:55:05 +0300

    Nav done all

--- src/sections/Nav.js
+++ src/sections/Nav.js
@@ -5,14 +5,61 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 import { FaBars } from "react-icons/fa";
 export const Nav = () => {
   const [dropdown, setDropdown] = useState(false);
-  return (
-    <div className="flex w-screen justify-between font-medium fixed bg-white">
-      <img src={logo} className="h-16 ml-6 pr-14"></img>
+  const [mobile, setMobile] = useState(false);
 
-      <div className="flex justify-center align-center hidden lg:flex">
-        <a className="h-16 ml-6 pt-6">Acasa</a>
-        <a className="h-16 ml-6 pt-6">Despre Noi</a>
-        <div className="h-16 ml-4 pt-6">
+  return (
+    <>
+      <div className="flex w-screen justify-between font-medium fixed bg-white z-10">
+        <img src={logo} className="h-16 ml-6 pr-14"></img>
+
+        <div className="flex justify-center align-center hidden lg:flex">
+          <a className="h-16 ml-6 pt-6">Acasa</a>
+          <a className="h-16 ml-6 pt-6">Despre Noi</a>
+          <div className="h-16 ml-4 pt-6">
+            <button
+              onClick={() => {
+                setDropdown(!dropdown);
+              }}
+              className="flex"
+            >
+              Servicii
+              <RiArrowDropDownLine className="pt-1" />
+            </button>
+            {dropdown && (
+              <div className="absolute rounded-b-lg flex flex-col">
+                <a className="pt-2">Creeare WebSite</a>
+                <a className="pt-2">Creeare Magazin Online</a>
+                <a className="pt-2">Creeare Aplicatie Web</a>
+                <a className="pt-2">Opttimizare SEO</a>
+              </div>
+            )}
+          </div>
+          <a className="h-16 ml-6 pt-6">Portofoliu</a>
+          <a className="h-16 ml-6 pt-6">Contact</a>
+        </div>
+        <div className="flex justify-center align-center pt-6 mr-10  hidden lg:flex">
+          <p className="mr-6">+40720404104</p>
+          <Button text="Hai Sa Vorbim"></Button>
+        </div>
+        <button
+          onClick={() => {
+            setMobile(!mobile);
+          }}
+          className="flex"
+        >
+          <FaBars
+            className="h-14 mr-6 lg:hidden"
+            style={{
+              color: "gray",
+            }}
+          />
+        </button>
+      </div>
+      {mobile && (
+        <div className="h-screen w-screen flex flex-col justify-evenly items-center fixed text-center text-xl font-bold lg:hidden">
+          <a className="h-16 ml-6 pt-6">Acasa</a>
+          <a className="h-16 ml-6 pt-6">Despre Noi</a>
+
           <button
             onClick={() => {
               setDropdown(!dropdown);
@@ -23,24 +70,20 @@ export const Nav = () => {
             <RiArrowDropDownLine className="pt-1" />
           </button>
           {dropdown && (
-            <div className="absolute rounded-b-lg flex flex-col">
-              <a className="pt-2">Creeare WebSite</a>
-              <a className="pt-2">Creeare Magazin Online</a>
-              <a className="pt-2">Creeare Aplicatie Web</a>
-              <a className="pt-2">Opttimizare SEO</a>
-            </div>
+            <>
+              <a className="pt-6">Creeare WebSite</a>
+              <a className="pt-6">Creeare Magazin Online</a>
+              <a className="pt-6">Creeare Aplicatie Web</a>
+              <a className="pt-6">Opttimizare SEO</a>
+            </>
           )}
+
+          <a className="h-16 ml-6 pt-6">Portofoliu</a>
+          <a className="h-16 ml-6 pt-6">Contact</a>
+          <p className="mr-6 mb-4">+40720404104</p>
+          <Button text="Hai Sa Vorbim"></Button>
         </div>
-        <a className="h-16 ml-6 pt-6">Portofoliu</a>
-        <a className="h-16 ml-6 pt-6">Contact</a>
-      </div>
-      <div className="flex justify-center align-center pt-6 mr-10  hidden lg:flex">
-        <p className="mr-6">+40720404104</p>
-        <Button text="Hai Sa Vorbim"></Button>
-      </div>
-      <button>
-        <FaBars className="h-14 mr-6" />
-      </button>
-    </div>
+      )}
+    </>
   );
 };
