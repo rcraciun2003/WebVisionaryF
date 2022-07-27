5e576f6dff7680388aefd07405cbd4f260f60d05
Aleksandruu <florin.mischie@student.upt.ro>
2022-07-27 16:50:08 +0300

    sadas

--- src/sections/HeaderHome.js
+++ src/sections/HeaderHome.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import headerbg from '../assets/images/header.svg';
-
+import React from "react";
+import headerbg from "../assets/images/header.svg";
 
 export const HeaderHome = () => {
   return (
     <section className="w-screen h-screen bg-no-repeat">
-        <img src={ headerbg } className="w-full"></img>
+      <img src={headerbg} className="w-full"></img>
     </section>
-    )
-}
+  );
+};
